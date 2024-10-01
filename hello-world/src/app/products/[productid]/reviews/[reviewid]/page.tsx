85366e9ce3186b0cae496b08c1e95d6eda4820a3
AliHassan487 <ah0720487@gmail.com>
2024-10-01 12:03:12 +0500

    custom not found page

--- hello-world/src/app/products/[productid]/reviews/[reviewid]/page.tsx
+++ hello-world/src/app/products/[productid]/reviews/[reviewid]/page.tsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({
   params,
 }: {
   params: { productid: string; reviewid: string };
 }) {
+  if(parseInt(params.reviewid) > 1000) {
+    notFound();
+  }
   return (
     <h1>
       Review {params.reviewid} for product {params.productid}
