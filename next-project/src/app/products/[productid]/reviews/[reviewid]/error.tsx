04e52655ba4a0684e159b63c2d71de508dd718c9
AliHassan487 <ah0720487@gmail.com>
2024-10-02 12:51:36 +0500

    Recovering From Errors

--- next-project/src/app/products/[productid]/reviews/[reviewid]/error.tsx
+++ next-project/src/app/products/[productid]/reviews/[reviewid]/error.tsx
@@ -1,4 +1,14 @@
 "use client";
-export default function ErrorBoundary({ error }: { error: Error }) {
-  return <h1>{error.message}</h1>;
+export default function ErrorBoundary({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <h1>
+      {error.message} <button onClick={() => reset()}>Try Again</button>
+    </h1>
+  );
 }
